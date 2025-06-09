ab2489779aec1c1f2d536155e65f62b825ceb3a4
Roxana Solano <roxansolan@gmail.com>
2025-06-09 22:01:32 +0200

    add Fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,32 @@
-/* import { useState } from "react"; */
+import { useState, useEffect } from "react";
 import "../styles/App.scss";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://hp-api.onrender.com/api/characters")
+      .then((response) => response.json())
+      .then((data) => {
+        const cleanCharacters = data.map((character) => {
+          return {
+            id: character.name,
+            name: character.name,
+            image: character.image,
+            species: character.species,
+            house: character.house,
+            gender: character.gender,
+            alive: character.alive,
+            alternateNames: character.alternate_names,
+          };
+        });
+        setCharacters(cleanCharacters);
+      })
+      .catch((error) => {
+        console.error("Error fetching characters:", error);
+      });
+  }, []);
+
   return (
     <div className="app_container">
       <header className="header">
@@ -9,7 +34,24 @@ function App() {
       </header>
 
       <main className="main_contect">
-        <p>Bienvenidas al mundo de Harry Potter</p>
+        <h2>Lista de Personajes</h2>
+        <ul className="character-list">
+          {" "}
+          {characters.map((char) => (
+            <li key={char.id}>
+              <img
+                src={
+                  char.image ||
+                  "https://placehold.co/210x295/1a1a1d/ccc?text=No+Image"
+                }
+                alt={char.name}
+                width="150"
+              />
+              <h3>{char.name}</h3>
+              <p>{char.species}</p>
+            </li>
+          ))}
+        </ul>
       </main>
 
       <footer className="footer">
