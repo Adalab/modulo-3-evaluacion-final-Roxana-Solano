657b69336d331cc6d72a1320cf336407e37eaf92
Roxana Solano <roxansolan@gmail.com>
2025-06-09 20:17:31 +0200

    add basic structure and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
+/* import { useState } from "react"; */
 import "../styles/App.scss";
 
 function App() {
-  return <p>Escribe aquí tu código HTML</p>;
+  return (
+    <div className="app_container">
+      <header className="header">
+        <h1 className="title">Wizarding World Directory</h1>
+      </header>
+
+      <main className="main_contect">
+        <p>Bienvenidas al mundo de Harry Potter</p>
+      </main>
+
+      <footer className="footer">
+        <p>&copy; 2025 React Project by Roxana Solano</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
